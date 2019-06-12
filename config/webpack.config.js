93b96f244ee1c7a4a8eaf66ab56535f10515231c
ktc <ktc@protonmail.com>
2019-06-12 20:22:45 +0300

    fix

--- config/webpack.config.js
+++ config/webpack.config.js
@@ -344,7 +344,7 @@ module.exports = function(webpackEnv) {
             // smaller than specified limit in bytes as data URLs to avoid requests.
             // A missing `test` is equivalent to a match.
             {
-              test: [/\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/, /\.svg$/, /\.woff2/, /\.eot/, /\.ttf/, /\.otf/],
+              test: [/\.bmp$/, /\.gif$/, /\.jpe?g$/, /\.png$/, /\.svg$/, /\.woff2$/, /\.eot$/, /\.ttf$/, /\.otf$/],
               loader: require.resolve('url-loader'),
               options: {
                 limit: 9999999999999999,
